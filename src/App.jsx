bb746feed81ad07f3eef6ea159fef1b790d20fbc
Pratham Rajak <prathamrajak.1999@gmail.com>
2024-03-29 21:36:03 +0530

    pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Signup from "./pages/Signup";
+import Signout from "./pages/Signout";
 
 export default function App() {
   return (
-    <h6 className='text-red-500'>App</h6>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/Signin" element={<Signin/>} />
+        <Route path="/About" element={<About/>} />
+        <Route path="/Profile" element={<Profile/>} />
+        <Route path="/Signup" element={<Signup/>} />
+        <Route path="/Signout" element={<Signout/>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
